d65f7d3f706e18216692291e1aabc07aadfd0917
jemblonganvalley <jemblonganvalley@gmail.com>
2024-10-19 23:51:44 +0700

    update

--- src/features/guest/list.js
+++ src/features/guest/list.js
@@ -3,9 +3,24 @@ const db = require("../../conn")
 
 async function guest_list(req = request, res = response){
     try {
-        const result = await db.guests.findMany()
+        const {page = 1, limit = 10} = req.query
+
+        // pagination
+        const take = parseInt(limit)
+        const skip = ( parseInt(page) - 1 ) * take
+
+        const result = await db.guests.findMany({
+            take : take,
+            skip : skip,
+        })
+
+        const total_data = await db.guests.count()
+
         return res.status(200).json({
             success : true,
+            current_page : parseInt(page),
+            total_page : Math.ceil(total_data / limit),
+            total_data : total_data,
             query : result
         })
 
